35cedcc1c00739a8942bb2f209684f61539b39f9
Ashish Kushwaha <221301739+ashishkushwaha138@users.noreply.github.com>
2025-09-20 01:47:19 +0530

    Lecture 1

---
+++ 2.Data Type.py
@@ -0,0 +1,21 @@
+#Data Type
+#Integer,String,Float,Boolean,None
+
+name = "Hii"
+name1 ='Hii'
+name2 ="""Hii"""
+
+print(name)
+print(name1)
+print(name2)
+
+#string data type can be written is three ways
+
+age = 17
+old = False
+a = None 
+
+print (type(old)) #boolen
+print (type(a))#None
+
+#True or False/None is a Keyword
---
+++ 3.Sum.py
@@ -0,0 +1,7 @@
+a = 5
+b = 2
+print (a+b) 
+a1 = 5
+b2 = 2
+diff = a1-b2
+print(diff)
