f0cd412137236d073454020330dcd37cea55d619
Ashish Kushwaha <221301739+ashishkushwaha138@users.noreply.github.com>
2025-09-20 13:26:02 +0530

    Lecture-2

---
+++ Lecture-2/3.String Function.py
@@ -0,0 +1,8 @@
+str = "i was to Studying Form to the Python"
+print(str.endswith("Python")) #returns true If string ends with Substr
+print (str.capitalize())
+print (str) #Second Time Captilize Not Work
+
+print(str.replace("Python", "Javascript")) #Replace
+print(str.find("o")) #Find
+print(str.count("to"))
---
+++ Lecture-2/Lets Practice.py
@@ -0,0 +1,4 @@
+#wRITE AN PROGRAM INPUT USER'S FIRST NAME &PRINT ITS LENGTH
+
+name = input("First Name :")
+print("Length Of a Name Is:", len(name))
