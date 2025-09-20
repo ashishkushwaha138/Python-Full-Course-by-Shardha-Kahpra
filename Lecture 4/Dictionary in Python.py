4fed8c81b88ccab5b017328aa7e39deadda54d6d
Ashish Kushwaha <221301739+ashishkushwaha138@users.noreply.github.com>
2025-09-20 21:33:54 +0530

    Lecture-4

---
+++ Lecture 4/Dictionary in Python.py
@@ -0,0 +1,18 @@
+#Dictionary are used to store data values in key:value pairs
+#List and Tuples also use in Dictionary
+#value me sare data type acceptable hai 
+#Dictionary are mutable
+#keys me list or dictionary use nahi ho sakta 
+
+
+info = {
+"key" : "value",
+"name" : "Ashish",
+"age" : 17,
+"marks" : 99.9, 
+"pass" : True, 
+
+}
+
+print(info)
+print(type(info))
